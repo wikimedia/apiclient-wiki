3bfe06748dcc5adf9987789ce44052906b9ff8b4
Evan Prodromou <evan@prodromou.name>
2020-09-04 14:04:32 -0400

    Start the OAuth authorisation process

--- js/apiclient.js
+++ js/apiclient.js
@@ -1,11 +1,16 @@
+// PKCE code Copyright (c) 2019 Aaron Parecki
+// https://github.com/aaronpk/pkce-vanilla-js
+
 const root = 'https://api.wikimedia.org/core/v1/wikipedia/en/'
+const authorize = 'https://meta.wikimedia.org/w/rest.php/oauth2/authorize'
 const server = 'https://apiclient.wiki/'
 
 const routes = [
   [new RegExp('^/$'), function(match) { fetchPage('Main Page')}],
   [new RegExp('^/index$'), function(match) { fetchPage('Main Page')}],
   [new RegExp('^/index.html$'), function(match) { fetchPage('Main Page')}],
-  [new RegExp('^/page/(.*)$'), function(match) { fetchPage(match[1]) }]
+  [new RegExp('^/page/(.*)$'), function(match) { fetchPage(match[1]) }],
+  [new RegExp('^/callback$'), function(match) { endLogin() }],
 ]
 
 const fetchPage = function(pageTitle) {
@@ -48,6 +53,67 @@ const goHome = function() {
   routeTo('/')
 }
 
+const startLogin = function() {
+  let clientID = getClientID()
+  let pkce = makePKCE()
+  savePKCE(pkce)
+  let params = {
+    client_id: clientID,
+    response_type: "code",
+    redirect_uri: `${server}callback`,
+    state: getPath(),
+    code_challenge: pkce.codeChallenge,
+    code_challenge_method: "S256"
+  }
+  let str = $.param(params)
+  let url = `${authorize}?${str}`
+  window.location = url
+  return false
+}
+
+const getClientID = function() {
+  return $("#client-id").text()
+}
+
+const getPath = function() {
+  return document.location.pathname
+}
+
+const makePKCE = function() {
+  let pkce = {
+    state: generateRandomString(),
+    codeVerifier: generateRandomString()
+  }
+  pkce.codeChallenge = pkceChallengeFromVerifier(pkce.codeVerifier)
+  return pkce
+}
+
+const savePKCE = function(pkce) {
+  localStorage.setItem("pkce_state", pkce.state)
+  localStorage.setItem("pkce_code_verifier", pkce.codeVerifier)
+}
+
+const generateRandomString = function() {
+  var array = new Uint32Array(28)
+  window.crypto.getRandomValues(array)
+  return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('')
+}
+
+// Base64-urlencodes the input string
+function base64urlencode(str) {
+    // Convert the ArrayBuffer to string using Uint8 array to conver to what btoa accepts.
+    // btoa accepts chars only within ascii 0-255 and base64 encodes them.
+    // Then convert the base64 encoded to base64url encoded
+    //   (replace + with -, replace / with _, trim trailing =)
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(str)))
+        .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+}
+
+function pkceChallengeFromVerifier(v) {
+    hashed = sha256(v)
+    return base64urlencode(hashed)
+}
+
 $(document).ready(function() {
   $(window).on('popstate', function(event) {
     if (event && event.originalEvent && event.originalEvent.state && event.originalEvent.state.key) {
@@ -58,5 +124,7 @@ $(document).ready(function() {
   })
   $('.navbar-brand').click(goHome)
   $('.navbar-home').click(goHome)
-  routeTo(document.location.pathname)
+  $('.navbar-login').click(startLogin)
+  let path = getPath()
+  routeTo(path)
 })
